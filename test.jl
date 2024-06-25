4eb513d96b6324ed6874239fc52a946bdf5469b0
alexp616 <pana5@bxscience.edu>
2024-06-24 18:58:16 -0700

    cleaned up files

--- test.jl
+++ test.jl
@@ -1,13 +1,1 @@
-using CUDA
-using BenchmarkTools
-
-
-result = CuArray(rand(Int32, (10000000, 6)))
-
-function sortByCol(arr, col)
-    arr = arr[sortperm(arr[:, col]), :]
-end
-
-
-
-@btime sortByCol(result, 6)
+println("hello world")
