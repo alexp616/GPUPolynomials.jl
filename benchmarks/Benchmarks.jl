adfcdffcab8adf6d53d679229ca7c6f1dfbe6bae
alexp616 <alpan@ucsd.edu>
2024-07-10 01:12:11 -0700

    added capability to generate benchmarks

--- benchmarks/Benchmarks.jl
+++ benchmarks/Benchmarks.jl
@@ -4,17 +4,19 @@ using Oscar
 
 # include("../experimental stuff/TrivialMultiply.jl")
 include("../src/Delta1.jl")
+include("../benchmarks/random_polynomial_generator.jl")
 
 # for the Meta.parse thing to work, these have to be global varibles
 # admittedly, this is a bit of a hack.
 R, (x,y,z,w) = polynomial_ring(GF(5),["x","y","z","w"])
 
+
 function read_benchmarks()
-  lines = readlines("benchmarks.txt")
+    lines = readlines("benchmarks/randompolynomials.txt")
 
-  polynomials = eval.(Meta.parse.(lines))
+    polynomials = eval.(Meta.parse.(lines))
 
-  polynomials
+    polynomials
 end
 
 lift_to_ZZ(p) = map_coefficients(x -> lift(ZZ,x),p)
@@ -36,6 +38,7 @@ function convert_to_gpu_representation(p)
   (coeffs_as_int64arr,exponent_mat)
 end
 
+
 function benchmarks_oscar()
     polys = read_benchmarks()
 
@@ -45,64 +48,92 @@ function benchmarks_oscar()
         return lifted ^ 5
     end
     # prime the jitter
-    println("PRIMING THE JITTER:")
-    @time polys[1]^4
+    polys[1]^4
     lift1 = lift_to_ZZ(polys[1])
-    @time lift1^5
+    lift1^5
 
     answers = []
     
-    println("TESTING:")
-    i = 1
-    for p in polys
-        println("Benchmark $i:")
-        i = i + 1
+    println("BENCHMARKING OSCAR...")
+    # i = 1
+    open("benchmarks/oscar_benchmarks.txt", "w") do file
+        redirect_stdout(file) do
+            println("OSCAR BENCHMARKS")
+            for p in polys
+                # println("Benchmark $i:")
+                # i = i + 1
+                @time answer = oscar_delta1(p, 5)
 
-        @time answer = oscar_delta1(p, 5)
-
-        push!(answers,answer)
+                push!(answers,answer)
+            end
+        end
     end
+    println("BENCHMARKING OSCAR FINISHED")
 
+    # display(answers)
     answers
 end
 
-function benchmarks_gpu()
+
+function benchmarks_gpu(samples)
     polys = read_benchmarks()
     gpu_data = convert_to_gpu_representation.(polys)
 
     pregen = pregen_delta1(4, 5)
 
+    h = HomogeneousPolynomial([1, 2, 3, 4], [0 0 0 4; 0 0 4 0; 0 4 0 0; 4 0 0 0])
     # prime the jitter
-    println("PRIMING THE JITTER:")
-    h = HomogeneousPolynomial(gpu_data[1][1],gpu_data[1][2])
-
-    CUDA.@time delta1(h, 5, pregen)
+    # warming up the gpu even more
+    for i in 1:10
+        delta1(h, 5, pregen)
+    end
 
     answers = []
   
-    println("TESTING:")
+    println("BENCHMARKING FFT...")
     i = 1
-    for data in gpu_data 
-        println("Benchmark $i:")
-        i = i + 1
-        
-        p = HomogeneousPolynomial(data[1],data[2])
-
-        CUDA.@time answer = delta1(p, 5, pregen)
-        push!(answers,answer)
+    numTermsArray = zeros(1:samples)
+    open("benchmarks/fft_benchmarks.txt", "w") do file
+        redirect_stdout(file) do
+            println("FFT BENCHMARKS")
+            for data in gpu_data 
+                # println("Benchmark $i:")
+                # i = i + 1
+                
+                p = HomogeneousPolynomial(data[1],data[2])
+                numTermsArray[i] = length(p.coeffs)
+                CUDA.@time answer = delta1(p, 5, pregen)
+                push!(answers,answer)
+                i += 1
+            end
+        end
     end
+    println("BENCHMARKING FFT FINISHED")
 
+    open("benchmarks/num_terms.txt", "w") do file
+        redirect_stdout(file) do
+            println("NUM TERMS")
+            for i in eachindex(numTermsArray) 
+                # println("Benchmark $i:")
+                # i = i + 1
+                println(Int(numTermsArray[i]))
+            end
+        end
+    end
     answers
 end
 
-function run_all_benchmarks()
 
-  println("--------------GPU-------------")
-  benchmarks_gpu()
-  println("-------------Oscar------------")
-#   benchmarks_oscar()
+function run_all_benchmarks(numSamples)
+    RandomPolynomialGenerator.run(numSamples)
+    println("--------------GPU-------------")
+    # Because I'm a horrible programmer, benchmarks_gpu also writes the 
+    # corresponding number of terms for each benchmark
+    benchmarks_gpu(numSamples)
+    println("-------------Oscar------------")
+    benchmarks_oscar()
 
-  nothing # don't spam the terminal
+    nothing # don't spam the terminal
 end
 
 end
---
+++ benchmarks/random_polynomial_generator.jl
@@ -0,0 +1,21 @@
+module RandomPolynomialGenerator
+
+include("../src/Delta1.jl")
+
+function generate_random_polynomials(numVars, modulus, numPolynomials, outputFile = "benchmarks/randompolynomials.txt")
+    maxTerms = binomial(numVars + numVars - 1, numVars)
+    open(outputFile, "w") do file
+        redirect_stdout(file) do
+            for _ in 1:numPolynomials
+                numTerms = rand(1:maxTerms)
+                println(pretty_string(random_homogeneous_polynomial(numVars, numTerms, modulus)))
+            end
+        end
+    end
+end
+
+function run(numSamples)
+    generate_random_polynomials(4, 5, numSamples)
+end
+
+end
