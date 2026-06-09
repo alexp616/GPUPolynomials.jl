c7a6be2090f88d9e75e8e11a377cfc8d20881718
JJ Garzella <jjgarzella@gmail.com>
2026-06-09 23:29:43 +0000

    harden: guard GPU tests behind CUDA.functional

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,17 @@
 using Test
+using CUDA
 using Oscar
 # include("../src/GPUPolynomials.jl")
 # using .GPUPolynomials
 using GPUPolynomials
 
 @testset "GPUPolynomials.jl" begin
-    include("CuZZPolyRingElemTests.jl")
-    include("CuZZMPolyRingElemTests.jl")
-    include("CufpMPolyRingElemTests.jl")
+    if CUDA.functional()
+        include("CuZZPolyRingElemTests.jl")
+        include("CuZZMPolyRingElemTests.jl")
+        include("CufpMPolyRingElemTests.jl")
+    else
+        @info "Skipping GPU tests because CUDA.functional() is false"
+        @test true
+    end
 end
